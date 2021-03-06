5561ed3659ef4680d3e0e638e11af0cf1d8f669a
fahad-zakir <fahadzakir81@gmail.com>
2021-03-06 14:48:58 -0600

    updated-server-index

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
 import express from 'express';
-import bodyparser from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+//mb for sending images which can be large in size, in case
+//setting up bodyParser so you can properly send request
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://abumusa1981:fahad0542@cluster0.zgqfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//setting up mongodb atlas to setup db now setup on their site and get credential
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);

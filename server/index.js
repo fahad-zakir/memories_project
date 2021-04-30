d81c2ca961badd3ab6b7eb18f41bc6239cc0b710
fahad-zakir <fahadzakir81@gmail.com>
2021-04-30 12:00:48 -0500

    started-routes-and-controllers

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 app.use('/posts', postRoutes);
+//means every routes inside of the postRoutes above will have to start with /posts
 
 //mb for sending images which can be large in size, in case
 //setting up bodyParser so you can properly send request
@@ -17,7 +18,7 @@ app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://abumusa1981:fahad0542@cluster0.zgqfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 //setting up mongodb atlas to setup db now setup on their site and get credential
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,6 +4,9 @@ import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
+// you are calling these functions in the argument
+// from the controllers folder where the logic is, 
+// so this simplifies and organizes the folder structure
 router.get('/', getPosts);
 router.post('/', createPost);
 

4be8acda45246ef12772561be906062084e4f8a5
fahad-zakir <fahadzakir81@gmail.com>
2021-05-01 17:34:45 -0500

    project-broke-from-tutorial

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,7 @@
-import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+import * as api from '../api/index.js';
+
 // means we import everything as api
 // so to fetchPosts all you do is this api.fetchPosts, since we will be
 // making a lot of api calls
@@ -8,16 +11,55 @@ import * as api from '../api';
 // payload is the data that stores a lot of our posts
 // redux thunk allows us to have another arrow function to make it asynchronous
 // in it we use dispatch to get access to it
-
 export const getPosts = () => async (dispatch) => {
-    try {
-        const { data } = await api.fetchPosts();
-        // getting response which always has the data object and storing it in data
-        // we are returning this from the back-end
-        // basically data represents the post
-    dispatch({ type: 'FETCH_ALL', payload: data });
-    //what's inside dispatch is an action
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const { data } = await api.fetchPosts();
+// getting response which always has the data object and storing it in data
+// we are returning this from the back-end
+// basically data represents the post
+    dispatch({ type: FETCH_ALL, payload: data });
+//what's inside dispatch is an action
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,21 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+// is a function that accepts a state and action
+// our state is going to be posts, so renaming state to posts above
 export default (posts = [], action) => {
-    // is a function that accepts a state and action
-    // our state is going to be posts, so renaming state to posts above
-    switch (action.type) {
-        case 'FETCH_ALL':
-            return action.payload;
-            // this is coming from the action dispatch.
-            // represents our posts
-        case 'CREATE':
-            return posts;
-        default:
-            return posts;
-    }
-}
-
-// you can export this to where it will be used
+  switch (action.type) {
+    case FETCH_ALL:
+      return action.paylo// this is coming from the action dispatch.
+ // represents our posts
+    case LIKE:
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case CREATE:
+      return [...posts, action.payload];
+    case UPDATE:
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
+    default:
+      return posts;
+  }
+};
+// you can export this for where it will be used
